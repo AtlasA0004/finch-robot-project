975f07f24d688c497ae2a100172bc5f62877b363
Atlas04 <atlasadamson@gmail.com>
2026-05-28 11:22:11 -0700

    v2

---
+++ src/main/java/com/birdbrain/finchdrawing/TesterFinch.java
@@ -0,0 +1,13 @@
+package com.finchdrawing;
+
+import com.birdbrain.Finch;
+
+public class TesterFinch {
+    public static void main(String[] args){
+        FinalFinch bird = new FinalFinch("A");
+
+        myFinch.setMove("F", 10, 50);
+    }
+
+
+}
