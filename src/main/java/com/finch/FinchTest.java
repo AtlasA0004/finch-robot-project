8bc14f7f54cf432338908e384009cbfa9e0fd14f
Atlas04 <atlasadamson@gmail.com>
2026-05-29 11:12:07 -0700

    a

--- src/main/java/com/finch/FinchTest.java
+++ src/main/java/com/finch/FinchTest.java
@@ -5,8 +5,16 @@ import com.birdbrain.Finch;
 public class FinchTest{
     public static void main(String[] args){
         Finch bird  = new Finch();
+        boolean metGoal = false;
 
         bird.setBeak(100, 100, 100);
+        bird.pause(1);
+
+        while(!metGoal){
+            bird.setMove("F", bird.getDistance(), 50);
+            metGoal =true;
+        }
+
 
 
     }
